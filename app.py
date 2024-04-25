a30207d8e41bff471483b48aa7453721cbdf37d3
Arpit P Jain <45422741+ArpitJain25@users.noreply.github.com>
2024-04-25 10:03:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,29 @@ import os
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+import google.generativeai as genai
+#from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 # Set page configuration
 st.set_page_config(page_title="RAG Application by APJ",
                    layout="wide",
                    page_icon="🧑‍⚕️")
 
-    
-# getting the working directory of the main.py
-working_dir = os.path.dirname(os.path.abspath(__file__))
+from IPython.display import display
+from IPython.display import Markdown
+import textwrap
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
 # sidebar for navigation
 with st.sidebar:
@@ -20,36 +35,66 @@ with st.sidebar:
                            icons=['activity', 'heart', 'person'],
                            default_index=0)
 
+# Processing Logic
+# PDF load and Split
+def get_pdf_text(pdf_docs):
+pdfloader = PyPDFLoader("/content/Modi-Ki-Guarantee-Sankalp-Patra-English.pdf")
+pdfpages = pdfloader.load_and_split()
+print(pdfpages[1].page_content)
+return pdfpages
 
-# Election 2024 - Bhartiya Janta Party (BJP) Manifesto Search
-if selected == 'BJP Manifesto':
+# Text Splitter
+def get_text_chunks(text):
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+context = "\n\n".join(str(p.page_content) for p in pdfpages)
+texts = text_splitter.split_text(context)
+return texts
 
-    # page title
-    st.title('Election 2024 - Bhartiya Janta Party (BJP) Manifesto Search')
+def get_vector_store(text_chunks, GOOGLE_API_KEY):
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GOOGLE_API_KEY)
+vector_store = Chroma.from_texts(texts, embeddings).as_retriever(search_kwargs={"k":5})
 
-    # getting the input data from the user
-    col1 = st.columns(1)
+def get_conversational_chain():
+model = ChatGoogleGenerativeAI(model="gemini-pro",google_api_key=GOOGLE_API_KEY,
+                             temperature=0.5)
 
-    with col1:
-        Question = st.text_input('Question')
+template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer.
+{context}
+Question: {question}
+Helpful Answer:"""
+QA_CHAIN_PROMPT = PromptTemplate.from_template(template)# Run chain
+qa_chain = RetrievalQA.from_chain_type(
+    llm=model,
+    retriever=vector_store,
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
+)
+return qa_chain
 
+  
+def user_input(user_question, api_key)
+result = qa_chain.invoke({"query": user_question})
+Markdown(result["result"])
+st.write("Reply: ", result["output_text"])
 
-    # code for Prediction
-    diab_diagnosis = ''
+def main():
+    st.header("AI clone chatbot💁")
 
-    # creating a button for Prediction
+    user_question = st.text_input("Ask a Question from the PDF Files", key="user_question")
 
-    if st.button('Check'):
+    if user_question and api_key:  # Ensure API key and user question are provided
+        user_input(user_question, api_key)
 
-        user_input = [Question]
+  
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True, key="pdf_uploader")
+        if st.button("Submit & Process", key="process_button") and api_key:  # Check if API key is provided before processing
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks, api_key)
+                st.success("Done")
 
-        user_input = [float(x) for x in user_input]
-
-        diab_prediction = diabetes_model.predict([user_input])
-
-        if diab_prediction[0] == 1:
-            diab_diagnosis = 'The person is diabetic'
-        else:
-            diab_diagnosis = 'The person is not diabetic'
-
-    st.success(diab_diagnosis)
+if __name__ == "__main__":
+    main()
